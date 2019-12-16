eb4b3daaec635d4fc5f4c87627ab1521f1cf00d3
Tyler de Friess <tyler.defriess@gmail.com>
2019-12-16 08:18:10 -0500

    Fifth challenge done

--- index.js
+++ index.js
@@ -164,8 +164,11 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(inventory) {
+  const theLast = inventory.find((item, index) => {
+    return index === inventory.length - 1
+  })
+  return `This is a ${theLast.car_make} ${theLast.car_model}`
 }
 
 /**
